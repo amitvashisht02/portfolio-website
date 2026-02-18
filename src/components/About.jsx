403640ba9095165a911f01fd490d39e3b76ba15f
Amit Vashisht <amitvashisht18@gmail.com>
2026-02-18 14:18:22 +0530

    update in about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div className="section" id="about">
             <h2>About Me</h2>
-            <p>Hi, I'm Amit — a self-taught, aspiring software developer who loves turning ideas into clean and functional interfaces.
+            <p>Hi, I'm Amit — a self-taught, frontend developer who loves turning ideas into clean and functional interfaces.
                 <br></br>
                 <br></br>
                 I started learning web development on my own, and every day I’m building, breaking, and improving things to grow faster. I enjoy understanding concepts deeply and creating simple solutions that work smoothly.
@@ -11,7 +11,7 @@ export default function About() {
                 Right now, I’m learning HTML, CSS, JavaScript, and React, and I’m constantly practising by building small projects and exploring UI/UX along the way.
                 <br></br>
                 <br></br>
-                I’m currently searching for a frontend internship or junior developer role where I can learn from real-world challenges and contribute to meaningful work.</p>
+                I’m currently searching for a frontend developer role where I can learn from real-world challenges and contribute to meaningful work.</p>
         </div>
     );
 }
